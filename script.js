e7c69f677a2b04d7ae423dead8633a1543760641
CourageSean <seancourage23@gmail.com>
2021-04-07 11:04:16 +0200

    initial

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 const api_key = "efbbb302664f73b592e1d04eca61e7ec"
 const PORT = process.env.Port || 5000
   require("dotenv").config()
+
+const express = require("express")
+
+const app = express()
+
+app.listen(PORT,()=>{
+    console.log("listening")
+} )
+
 console.log(process.env)
