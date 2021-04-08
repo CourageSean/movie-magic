a7338f642b932dc665b0b3512cc68fcee3eed4b2
CourageSean <seancourage23@gmail.com>
2021-04-08 14:14:54 +0200

    n

--- script.js
+++ script.js
@@ -1,13 +1,86 @@
 const api_key = "efbbb302664f73b592e1d04eca61e7ec"
 const PORT = process.env.Port || 5000
   require("dotenv").config()
-
+const { response } = require("express")
 const express = require("express")
+const fetch = require("node-fetch")
+const genres = require("./data/genre.json")
 
 const app = express()
+app.set("view engine","ejs")
 
-app.listen(PORT,()=>{
+app.listen(3001,()=>{
     console.log("listening")
 } )
 
-console.log(process.env)
+app.use(express.static('public'))
+let genreType = ``
+
+let page_id ="2"
+
+const getData = async (genreType,page_id)=>{
+const response = await fetch(`https://api.themoviedb.org/3/discover/movie?with_genres=${genreType}&api_key=efbbb302664f73b592e1d04eca61e7ec&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page_id}`)
+
+const data = await response.json()
+
+ console.log(data.results[0].genre_ids)
+return data
+}
+
+const getDetail = async (movie_id)=>{
+  const response = await fetch(`https://api.themoviedb.org/3/movie/${movie_id}?api_key=efbbb302664f73b592e1d04eca61e7ec&language=en-US`)
+const detailData = await response.json()
+
+return detailData
+}
+
+getData("")
+.then((data)=>{
+  app.get("/",(req,res)=>{
+    res.render("pages/index",{data})
+    
+      })
+  
+
+      app.get("/:id",(req,res)=>{
+     
+          getDetail(req.params.id)
+          .then((detailData)=>{
+            res.render("pages/movieDetail",{detailData})
+            console.log(detailData.genres)
+          })
+         
+      } )
+
+  })
+
+
+ app.get("/action",(req,res)=>{
+    getData("28")
+    .then((data)=>{
+      res.render("pages/index",{data})
+ 
+  })
+
+}) 
+
+
+  app.get("/drama",(req,res)=>{
+    getData("18")
+    .then((data)=>{
+      res.render("pages/index",{data})
+     
+        
+    }) 
+  })
+
+
+  app.get("/movies/page/:nr",(req,res)=>{
+    let page_nr = String(req.params.nr) 
+    console.log(page_nr)
+getData("",page_nr)
+.then((data)=>{
+
+  res.render("pages/index",{data})
+})
+  })
