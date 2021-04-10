7638133eb436f38b2957208f2bff1d5d72a5e7de
CourageSean <seancourage23@gmail.com>
2021-04-11 00:55:54 +0200

    initial

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const api_key = "efbbb302664f73b592e1d04eca61e7ec"
+
 const PORT = process.env.Port || 5000
   require("dotenv").config()
 const { response } = require("express")
@@ -14,12 +14,13 @@ app.listen(process.env.Port || 5000,()=>{
 } )
 
 app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
 let genreType = ``
 
 let page_id ="2"
 
 const getData = async (genreType,page_id)=>{
-const response = await fetch(`https://api.themoviedb.org/3/discover/movie?with_genres=${genreType}&api_key=efbbb302664f73b592e1d04eca61e7ec&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page_id}`)
+const response = await fetch(`https://api.themoviedb.org/3/discover/movie?with_genres=${genreType}&api_key=${process.env.API_KEY}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${page_id}year=1`)
 
 const data = await response.json()
 
@@ -28,10 +29,17 @@ return data
 }
 
 const getDetail = async (movie_id)=>{
-  const response = await fetch(`https://api.themoviedb.org/3/movie/${movie_id}?api_key=efbbb302664f73b592e1d04eca61e7ec&language=en-US`)
+  const response = await fetch(`https://api.themoviedb.org/3/movie/${movie_id}?api_key=${process.env.API_KEY}&language=en-US`)
 const detailData = await response.json()
 
 return detailData
+}
+
+const getSearch = async (search)=>{
+
+const response = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&language=en-US&query=${search}&page=1&include_adult=false`)
+const data = await response.json()
+return data
 }
 
 getData("")
@@ -39,6 +47,7 @@ getData("")
   app.get("/",(req,res)=>{
     let page_nr = 1
     res.render("pages/index",{data,page_nr})
+    console.log(data.results)
     
       })
   
@@ -48,7 +57,7 @@ getData("")
           getDetail(req.params.id)
           .then((detailData)=>{
             res.render("pages/movieDetail",{detailData})
-            console.log(detailData.genres)
+            
           })
          
       } )
@@ -56,15 +65,21 @@ getData("")
   })
 
 
- app.get("/movies/action/:nr",(req,res)=>{
+ app.get("/movies/:action/:nr",(req,res)=>{
   let page_nr = req.params.nr
+  let page_genre = req.params.action
+  console.log(page_genre)
     getData("28",page_nr)
     .then((data)=>{
-      res.render("pages/index",{data,page_nr})
+      res.render("pages/index",{data,page_nr,page_genre})
  
   })
 
 }) 
+
+
+
+
 
 
   app.get("/movies/drama/:nr",(req,res)=>{
@@ -96,4 +111,22 @@ getData("",page_nr)
 
   res.render("pages/index",{data,page_nr})
 })
+  })
+
+  app.get("/movies/search/:word",(req,res)=>{
+    getSearch(req.params.word)
+    .then((data)=>{
+      console.log("here-2")
+      let page_nr =1
+      
+      res.render("pages/index",{data,page_nr})
+      
+    } )
+    
+  })
+  app.post("/search",(req,res)=>{
+    // console.log(req.body.search)
+    console.log("just here")
+  res.redirect(`movies/search/${req.body.search}`)
+  
   })
